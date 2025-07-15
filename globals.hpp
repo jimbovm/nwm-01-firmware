9b67443de0575865f0248662d86a71c588ee3383
James Brierley <jmb8710@gmail.com>
2025-07-15 16:01:35 +0100

    fixup! eliminate removed config parameter

--- globals.hpp
+++ globals.hpp
@@ -15,25 +15,22 @@ namespace nwm_01 {
 	struct config {
 
 		uint8_t revision;
-		uint8_t velocityCurve;
+		velocity::Curve velocityCurve;
 		uint8_t velocityCCReassign;
-		uint8_t arpeggiatorPattern;
-		uint8_t gatePolarity;
-		uint8_t triggerPolarity;
+		arpeggiator::Pattern arpeggiatorPattern;
+		gate::Polarity gatePolarity;
+		trigger::Polarity triggerPolarity;
 		uint8_t triggerDuration;
-		uint8_t clockDuration;
-		uint8_t manufacturerID;	
 		uint8_t lowNote;
-		uint8_t highNote;
+		OperatingMode operatingMode;
+		uint8_t clockDuration;
 		uint8_t receiveChannel;
-		uint8_t tuningTable[TUNING_LENGTH * 2];
-		uint8_t sequenceTable[SEQUENCE_LENGTH * 6];
+		uint8_t manufacturerID;
 		uint8_t internalClockTempo;
 		uint8_t forceInternalClock;
 		uint8_t inputClockPPQN;
 		uint8_t outputClockPPQN;
-		uint8_t reserved;
-		uint8_t endOfFile;
+		uint8_t tuning;
 	};
 
 	extern struct config _config;
