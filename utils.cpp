fc23d6137997f00415cea53b082bb10e07036716
James Brierley <jmb8710@gmail.com>
2025-07-17 19:56:18 +0100

    qsort held notes

--- utils.cpp
+++ utils.cpp
@@ -3,6 +3,7 @@
 #include "globals.hpp"
 
 #include <Arduino.h>
+#include <stdlib.h>
 
 namespace nwm_01 {
 
@@ -12,6 +13,12 @@ namespace nwm_01 {
 
 	bool expired(unsigned long epoch, int duration) {
 		return (micros() - epoch) >= duration;
+	}
+
+	inline int compareAscend(const void* a_, const void* b_) {
+		const int a = *((const int*) a_);
+		const int b = *((const int*) b_); 
+		return (a > b) - (b < a);
 	}
 
 	uint8_t* getHeldCanonicalNotes(unsigned long long keysDown) {
@@ -33,6 +40,10 @@ namespace nwm_01 {
 
 			// mask out note just seen
 			keysDown ^= 1 << heldMidiNote;
+		}
+
+		if (_config.arpeggiatorPattern != arpeggiator::Pattern::RANDOM) {
+			qsort(heldNotes, _numberOfKeysDown, sizeof(uint8_t), compareAscend);
 		}
 
 		return heldNotes;
