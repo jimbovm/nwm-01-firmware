92a7e373230d8d589ad44325a28ecad7214e9f45
James Brierley <jmb8710@gmail.com>
2025-07-19 10:17:41 +0100

    fix indents

--- sysex.hpp
+++ sysex.hpp
@@ -5,10 +5,10 @@
 
 namespace nwm_01 {
 
-	void doSysex(uint8_t array[], unsigned size);
-    void setIndividualParameter(uint8_t parameter, uint8_t value);
-    void setTuning(uint8_t array[], unsigned size);
-    bool validManufacturerId(uint8_t array[], unsigned size);
-    bool validDeviceId(uint8_t array[], unsigned size);
+void doSysex(uint8_t array[], unsigned size);
+	void setIndividualParameter(uint8_t parameter, uint8_t value);
+	void setTuning(uint8_t array[], unsigned size);
+	bool validManufacturerId(uint8_t array[], unsigned size);
+	bool validDeviceId(uint8_t array[], unsigned size);
 }
 #endif
